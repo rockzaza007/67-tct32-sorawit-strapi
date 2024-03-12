cb7fa127b2283f6fdeb436d8a40ec4738ca80ac9
Sorawit Hanumasm <rockzaza007@gmail.com>
2024-03-12 10:39:58 +0700

    Add service encrytion and decrytion

--- src/api/student/content-types/student/lifecycles.js
+++ src/api/student/content-types/student/lifecycles.js
@@ -1,60 +1,33 @@
 const crypto = require('crypto');
-
 const algorithm = 'aes-256-cbc';
 const key = Buffer.from(process.env.SECRET_KEY); // Should be a 32-byte key for aes-256
 const iv = process.env.SECRET_KEY_IV; // Should be a 16-byte IV for aes-256-cbc
+const api = 'api::student.student';
+const encrypt = async (data) => {return strapi.service(api).encryptPhoneNumber(data)}
+const decrypt = async (data) => {return strapi.service(api).decryptPhoneNumber(data)}
 
-const encryptPhoneNumber = (phoneNumber) => {
-  let encryptedPhoneNumber = '';
-  // Split the phone number into pairs of two characters
-  for (let i = 0; i < phoneNumber.length; i += 2) {
-    const chunk = phoneNumber.slice(i, i + 2);
-    // Encrypt each chunk and append to the result
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
-    let encryptedChunk = cipher.update(chunk, 'utf8', 'hex');
-    encryptedChunk += cipher.final('hex');
-    encryptedPhoneNumber += encryptedChunk;
-  }
-  return encryptedPhoneNumber;
-};
-
-const decryptPhoneNumber = (encryptedPhoneNumber) => {
-  let decryptedPhoneNumber = '';
-  // Split the encrypted phone number into pairs of encrypted chunks
-  for (let i = 0; i < encryptedPhoneNumber.length; i += 32) { // Assuming each chunk is 32 characters long
-    const encryptedChunk = encryptedPhoneNumber.slice(i, i + 32); // Assuming each chunk is 32 characters long
-    // Decrypt each chunk and append to the result
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
-    let decryptedChunk = decipher.update(encryptedChunk, 'hex', 'utf8');
-    decryptedChunk += decipher.final('utf8');
-    decryptedPhoneNumber += decryptedChunk;
-  }
-  return decryptedPhoneNumber;
-};
 
 module.exports = {
   async beforeCreate(event) {
     console.log('beforeCreate', event.params);
-    event.params.data.PhoneNumber = encryptPhoneNumber(event.params.data.PhoneNumber);
+    const phoneNumber = event.params.data.PhoneNumber;
+    event.params.data.PhoneNumber = await encrypt(phoneNumber);
   },
   async beforeUpdate(event) {
     console.log('beforeUpdate', event.params.data);
-    event.params.data.PhoneNumber = encryptPhoneNumber(event.params.data.PhoneNumber);
+    const phoneNumber = event.params.data.PhoneNumber;
+    event.params.data.PhoneNumber = await encrypt(phoneNumber);
   },
   async afterFindMany(event) {
     console.log('afterFindMany', event.result);
-    event.result.forEach(item => {
-      if (item.PhoneNumber) {
-        item.PhoneNumber = decryptPhoneNumber(item.PhoneNumber);
-        console.log('afterFindMany :', item.PhoneNumber);
-      }
-    });
+    event?.result?.map(async result => {
+      result.PhoneNumber = await decrypt(result.PhoneNumber);
+    })
   },
   async afterFindOne(event) {
     console.log('afterFindOne', event.result);
     if (event.result && event.result.PhoneNumber) {
-      event.result.PhoneNumber = decryptPhoneNumber(event.result.PhoneNumber);
-      console.log('afterFindOne :', event.result.PhoneNumber);
+      event.result.PhoneNumber = await decrypt(event.result.PhoneNumber);
     }
   },
 };
